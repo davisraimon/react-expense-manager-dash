dae5a196563ff54a693f060b7240cbf238e81074
Davis Raimon <davisraimon@gmail.com>
2020-07-19 23:45:32 +0530

    pdf reader minor code addition commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
 import "./App.css";
 import ListExpense from "./components/ListExpense";
-import { Document, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
-  const options = {
-    cMapUrl: "cmaps/",
-    cMapPacked: true,
-  };
-  function readPDF() {
-    alert("asd");
-  }
+
   return (
     <div className="Login">
       <h1 className="App-header">Expense Calculator</h1>
@@ -19,7 +11,6 @@ function App() {
         <div className="row">
           <div className="col-sm column-attr-center">
             <ListExpense></ListExpense>
-            {/* <Document file="/sample.pdf" onLoadSuccess={readPDF} options={options}></Document> */}
           </div>
         </div>
       </div>
