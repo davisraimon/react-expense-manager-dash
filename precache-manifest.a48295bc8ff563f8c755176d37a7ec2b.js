1d1023c1634389968dacb35699227cdd320fb1f5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-20 18:31:50 +0000

    Deploy davisraimon/react-expense-manager-dash to github.com/davisraimon/react-expense-manager-dash.git:gh-pages

---
+++ precache-manifest.a48295bc8ff563f8c755176d37a7ec2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55aab10ebc284ba66f2319d9a676e87c",
+    "url": "/react-expense-manager-dash/index.html"
+  },
+  {
+    "revision": "7d5b2b3ba6be0b8f9de0",
+    "url": "/react-expense-manager-dash/static/css/main.ca763671.chunk.css"
+  },
+  {
+    "revision": "c3b8059443ade2137ba1",
+    "url": "/react-expense-manager-dash/static/js/2.373b6aeb.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/react-expense-manager-dash/static/js/2.373b6aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5b2b3ba6be0b8f9de0",
+    "url": "/react-expense-manager-dash/static/js/main.8e3c0422.chunk.js"
+  },
+  {
+    "revision": "8c75871ee9ea777780a4",
+    "url": "/react-expense-manager-dash/static/js/runtime-main.1232947e.js"
+  }
+]);
